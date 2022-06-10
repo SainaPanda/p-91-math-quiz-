8b7103483650869b232b9cb9c053b83059d5472b
SainaPanda <88354134+SainaPanda@users.noreply.github.com>
2022-06-10 12:06:31 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ document.getElementById("player2_score").innerHTML = player2_score ;
 document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
 document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
 
-function send() {
+function mul() {
 	number1 = document.getElementById("number1").value;
 	number2 = document.getElementById("number2").value;
 	actual_answer = parseInt(number1) * parseInt(number2);
@@ -22,6 +22,51 @@ function send() {
 
 
     question_number = "<h4>" + number1 + " X "+ number2 +"</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row =  question_number + input_box + check_button ; 
+    document.getElementById("output").innerHTML = row;
+	document.getElementById("number1").value = "";
+	document.getElementById("number2").value = "";
+}
+function div() {
+	number1 = document.getElementById("number1").value;
+	number2 = document.getElementById("number2").value;
+	actual_answer = parseInt(number1) / parseInt(number2);
+	console.log(actual_answer);
+
+
+    question_number = "<h4>" + number1 + " / "+ number2 +"</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row =  question_number + input_box + check_button ; 
+    document.getElementById("output").innerHTML = row;
+	document.getElementById("number1").value = "";
+	document.getElementById("number2").value = "";
+}
+function add() {
+	number1 = document.getElementById("number1").value;
+	number2 = document.getElementById("number2").value;
+	actual_answer = parseInt(number1) + parseInt(number2);
+	console.log(actual_answer);
+
+
+    question_number = "<h4>" + number1 + " + "+ number2 +"</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row =  question_number + input_box + check_button ; 
+    document.getElementById("output").innerHTML = row;
+	document.getElementById("number1").value = "";
+	document.getElementById("number2").value = "";
+}
+function sub() {
+	number1 = document.getElementById("number1").value;
+	number2 = document.getElementById("number2").value;
+	actual_answer = parseInt(number1) - parseInt(number2);
+	console.log(actual_answer);
+
+
+    question_number = "<h4>" + number1 + " - "+ number2 +"</h4>";
     input_box = "<br>Answer : <input type='text' id='input_check_box'>";
     check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
     row =  question_number + input_box + check_button ; 
